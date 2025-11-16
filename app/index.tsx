25a116564aea4a246da04e4b33b2f6f02d00a49c
dev2820 <second28200@naver.com>
2025-11-17 00:43:58 +0900

    fix: message Input moved following keyboard

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,25 @@
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 import { MessageInput } from "@/components/message-input";
 import { MessageView } from "@/components/message-view";
-import { KeyboardStickyView } from "react-native-keyboard-controller";
+import {
+  KeyboardAwareScrollView,
+  KeyboardStickyView,
+} from "react-native-keyboard-controller";
 
 export default function HomeScreen() {
+  const safeInset = useSafeAreaInsets();
   return (
     <SafeAreaView className="flex-1 bg-background">
-      <MessageView />
-      <KeyboardStickyView offset={{ closed: 0, opened: 8 }}>
+      <KeyboardAwareScrollView className="flex-grow">
+        <MessageView />
+      </KeyboardAwareScrollView>
+      <KeyboardStickyView
+        offset={{ closed: 0, opened: safeInset.bottom }}
+        className="bg-background"
+      >
         <MessageInput />
       </KeyboardStickyView>
     </SafeAreaView>
